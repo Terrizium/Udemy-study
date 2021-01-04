ca205c1a4280f843fd3645a3805812f3c2357496
Aleksey <saymongti1@gmail.com>
2021-01-04 22:58:56 +0700

    add descripion some functions

---
+++ study.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/*function showFirstMessage(text) {
+    console.log(text);
+}
+
+showFirstMessage(5);
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));
+console.log(calc(4, 13));
+console.log(calc(4, 5));
+
+
+отдельный разбор строчных функций
+
+const a = 'text';
+
+console.log(a.toLowerCase());
+console.log(a.toUpperCase());
+
+let fruit = 'Some fruit';
+
+console.log(fruit.indexOf('r'));
+
+let logg = 'Hello World';
+
+console.log(logg.slice(5, 10)); //вырезание от и до индкса
+
+console.log(logg.substring(2, 5));
+
+console.log(logg.substr(6, 5)); //вырезание по количеству символов
+*/
+
+const num = 12.2;
+console.log(Math.round(num)); //округлние
+
+const test = '12.2px';
+console.log(parseInt(test)); //конвертация в инт
+console.log(parseFloat(test)); //возвращает флоат (с плавающей точкой)
+
